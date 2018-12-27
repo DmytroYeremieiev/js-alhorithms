760b29d48440093cc8a071f5dd16e677e89605d5
DmytroYeremieiev <dmytro.yeremieiev@gmail.com>
2018-12-27 23:24:02 +0200

    -added tasks -tuned up unit test on change restart

--- JS/tests/noname-tasks-spec.ts
+++ JS/tests/noname-tasks-spec.ts
@@ -1,12 +1,11 @@
 import { expect } from 'chai';
-import 'mocha';
 
 import * as nonameTasks from '../src/noname-tasks';
 
 describe('noname', () => {
   it('returns an argument unchanged', () => {
-    var result = nonameTasks.noname('gosh')
-    expect(result).to.equal('gosh');
+    var result = nonameTasks.noname('goshs');
+    expect(result).to.equal('goshs');
   });
 })
 
