f621c872ac21addfc25cffb75633739a966f4724
DmytroYeremieiev <dmytro.yeremieiev@gmail.com>
2018-12-28 21:01:39 +0200

    - first task

--- JS/src/noname-tasks.ts
+++ JS/src/noname-tasks.ts
@@ -1,5 +1,11 @@
-export {noname}
+export {sumAll}
  
-function noname(name:string) {
-  return name;
+// We'll pass you an array of two numbers. Return the sum of those two numbers plus the sum of all the numbers between them.
+// The lowest number will not always come first.
+function sumAll(arr:[number, number]) {
+  let min = Math.min(...arr), max = Math.max(...arr), sum = 0;
+  for (let i = min; i <= max; i++) {
+    sum += i;
+  }
+  return sum;
 }
--- JS/tests/noname-tasks-spec.ts
+++ JS/tests/noname-tasks-spec.ts
@@ -2,10 +2,26 @@ import { expect } from 'chai';
 
 import * as nonameTasks from '../src/noname-tasks';
 
-describe('noname', () => {
-  it('returns an argument unchanged', () => {
-    var result = nonameTasks.noname('goshs');
-    expect(result).to.equal('goshs');
+describe('sumAll', () => {
+  it('should return a number.', () => {
+    var result = nonameTasks.sumAll([1, 4]);
+    expect(result).to.be.a('number');
+  });
+  it('should return 10', () => {
+    var result = nonameTasks.sumAll([1, 4]);
+    expect(result).to.equal(10);
+  });
+  it('should return 10', () => {
+    var result = nonameTasks.sumAll([4, 1]);
+    expect(result).to.equal(10);
+  });
+  it('should return 45', () => {
+    var result = nonameTasks.sumAll([5, 10]);
+    expect(result).to.equal(45);
+  });
+  it('should return 45', () => {
+    var result = nonameTasks.sumAll([10, 5]);
+    expect(result).to.equal(45);
   });
 })
 
