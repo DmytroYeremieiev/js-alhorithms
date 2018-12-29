359d35ba1b2f33a0b1f88f3248d12a43f1904018
DmytroYeremieiev <dmytro.yeremieiev@gmail.com>
2018-12-29 21:45:38 +0200

    - some tasks

--- JS/src/noname-tasks.ts
+++ JS/src/noname-tasks.ts
@@ -1,5 +1,70 @@
-export {sumAll, diffArray}
- 
+export {sumAll, diffArray, destroyer, whatIsInAName, spinalCase}
+
+//Convert a string to spinal case. Spinal case is all-lowercase-words-joined-by-dashes.
+function spinalCase(str:string) : string {
+  const resultArr : string[] = [];
+  for (let index=0; index<str.length; index++) {
+    let currentSymbol = str[index], previousSymbol = str[index-1];
+    if (currentSymbol === '_' || currentSymbol === ' ' || currentSymbol === '-') {
+      resultArr.push('-');
+      continue;
+    }
+    if (currentSymbol === currentSymbol.toUpperCase()) {
+      resultArr.push((index !== 0 && resultArr[resultArr.length-1] !== '-') ? '-'+currentSymbol.toLowerCase():currentSymbol.toLowerCase());
+    } else {
+      resultArr.push(currentSymbol.toLowerCase());
+    }
+  }
+  return resultArr.join('')
+}
+
+function matchAllPropertiesOfSourceObj(source:{[key:string]:any}, objToMatch: {[key:string]:any}) : boolean {
+  for (const key in source) {
+    if (source.hasOwnProperty(key)) {
+      if(!objToMatch[key] || objToMatch[key] !== source[key]){return false}
+    }
+  }
+  return true
+}
+
+// Make a function that looks through an array of objects (first argument) and 
+// returns an array of all objects that have matching name and value pairs (second argument). 
+// Each name and value pair of the source object has to be present in the object from the collection 
+// if it is to be included in the returned array.
+function whatIsInAName(collection: object[], source: object) : object[] {
+  // What's in a name?
+  const result : object[] = [];
+  // Only change code below this line
+  
+  for (let index = 0; index < collection.length; index++) {
+    if(matchAllPropertiesOfSourceObj(source, collection[index])){
+      result.push(collection[index]);
+    }
+  }
+  // Only change code above this line
+  return result;
+}
+
+whatIsInAName([
+  { first: "Romeo", last: "Montague" }, 
+  { first: "Mercutio", last: null }, 
+  { first: "Tybalt", last: "Capulet" }], 
+  { last: "Capulet" });
+
+// You will be provided with an initial array (the first argument in the destroyer function), 
+// followed by one or more arguments. 
+// Remove all elements from the initial array that are of the same value as these arguments.
+function destroyer(arr: any[], ...params: any[]) : any[] {
+  const result = []
+  for (let index = 0; index < arr.length; index++) {
+    if (!~params.indexOf(arr[index])) {
+      result.push(arr[index]);
+    }
+  }
+  return result;
+}
+
+
 // Compare two arrays and return a new array with any items only found in one
 // of the two given arrays, but not both. In other words, 
 // return the symmetric difference of the two arrays.
@@ -18,7 +83,6 @@ function diffArray(arr1: any[], arr2: any[]) : any[] {
   return newArr;
 }
 
-diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
 
 // We'll pass you an array of two numbers. Return the sum of those two numbers plus the sum of all the numbers between them.
 // The lowest number will not always come first.
--- JS/tests/noname-tasks-spec.ts
+++ JS/tests/noname-tasks-spec.ts
@@ -2,6 +2,61 @@ import { expect } from 'chai';
 
 import * as nonameTasks from '../src/noname-tasks';
 
+describe('spinalCase', () => {
+  it('should return a "this-is-spinal-tap"', () => {
+    var result = nonameTasks.spinalCase("This Is Spinal Tap");
+    expect(result).to.be.equal("this-is-spinal-tap");
+  });
+  it('should return a "this-is-spinal-tap"', () => {
+    var result = nonameTasks.spinalCase("thisIsSpinalTap");
+    expect(result).to.be.equal("this-is-spinal-tap");
+  });
+  it('should return a "the-andy-griffith-show"', () => {
+    var result = nonameTasks.spinalCase("The_Andy_Griffith_Show");
+    expect(result).to.be.equal("the-andy-griffith-show");
+  });
+  it('"Teletubbies say Eh-oh" should return a "teletubbies-say-eh-oh"', () => {
+    var result = nonameTasks.spinalCase("Teletubbies say Eh-oh");
+    expect(result).to.be.equal("teletubbies-say-eh-oh");
+  });
+  it('"AllThe-small Things" should return a "all-the-small-things"', () => {
+    var result = nonameTasks.spinalCase("AllThe-small Things");
+    expect(result).to.be.equal("all-the-small-things");
+  });
+});
+
+describe('whatIsInAName', () => {
+  it('should return [{ first: "Tybalt", last: "Capulet" }]', ()=>{
+    var result = nonameTasks.whatIsInAName([
+      { first: "Romeo", last: "Montague" }, 
+      { first: "Mercutio", last: null }, 
+      { first: "Tybalt", last: "Capulet" }], 
+      { last: "Capulet" }
+    );
+    expect(result).to.have.deep.members([{ first: "Tybalt", last: "Capulet" }]);
+  })
+  it('should return [{ "apple": 1 }, { "apple": 1 }, { "apple": 1, "bat": 2 }]', ()=>{
+    var result = nonameTasks.whatIsInAName([{ "apple": 1 }, { "apple": 1 }, { "apple": 1, "bat": 2 }], { "apple": 1 });
+    expect(result).to.have.deep.members([{ "apple": 1 }, { "apple": 1 }, { "apple": 1, "bat": 2 }]);
+  })
+  it('should return [{ "apple": 1, "bat": 2 }, { "apple": 1, "bat": 2, "cookie": 2 }]', ()=>{
+    var result = nonameTasks.whatIsInAName([{ "apple": 1, "bat": 2 }, { "bat": 2 }, { "apple": 1, "bat": 2, "cookie": 2 }], { "apple": 1, "bat": 2 });
+    expect(result).to.have.deep.members([{ "apple": 1, "bat": 2 }, { "apple": 1, "bat": 2, "cookie": 2 }]);
+  })
+  it('should return [{ "apple": 1, "bat": 2, "cookie": 2 }]', ()=>{
+    var result = nonameTasks.whatIsInAName([{ "apple": 1, "bat": 2 }, { "apple": 1 }, { "apple": 1, "bat": 2, "cookie": 2 }], { "apple": 1, "cookie": 2 });
+    expect(result).to.have.deep.members([{ "apple": 1, "bat": 2, "cookie": 2 }]);
+  });
+  it('should return [{ "apple": 1, "bat": 2 }, { "apple": 1, "bat": 2, "cookie":2 }]', ()=>{
+    var result = nonameTasks.whatIsInAName([{ "apple": 1, "bat": 2 }, { "apple": 1 }, { "apple": 1, "bat": 2, "cookie": 2 }, { "bat":2 }], { "apple": 1, "bat": 2 });
+    expect(result).to.have.deep.members([{ "apple": 1, "bat": 2 }, { "apple": 1, "bat": 2, "cookie":2 }]);
+  });
+  it('should return [{ "apple": 1, "bat": 2, "cookie": 2 }]', ()=>{
+    var result = nonameTasks.whatIsInAName([{"a": 1, "b": 2, "c": 3}], {"a": 1, "b": 9999, "c": 3});
+    expect(result).to.have.lengthOf(0);
+  });
+});
+
 describe('sumAll', () => {
   it('should return a number.', () => {
     var result = nonameTasks.sumAll([1, 4]);
@@ -23,7 +78,7 @@ describe('sumAll', () => {
     var result = nonameTasks.sumAll([10, 5]);
     expect(result).to.equal(45);
   });
-})
+});
 
 describe("diffArray", ()=>{
   it('should return an array.', ()=>{
@@ -64,5 +119,35 @@ describe("diffArray", ()=>{
     var result = nonameTasks.diffArray([1, "calf", 3, "piglet"], [7, "filly"]);
     expect(result).to.have.lengthOf(6).and.to.include.members([1, "calf", 3, "piglet", 7, "filly"]);
   })
-})
+});
 
+describe('destroyer', () => {
+  it('should return a [1, 1]', () => {
+    var result = nonameTasks.destroyer([1, 2, 3, 1, 2, 3], 2, 3);
+    expect(result).to.have.lengthOf(2).to.be.a('array').and.to.include.members([1, 1]);
+  });
+  it('should return a [1, 5, 1]', () => {
+    var result = nonameTasks.destroyer([1, 2, 3, 5, 1, 2, 3], 2, 3);
+    expect(result).to.have.lengthOf(3).and.to.include.members([1, 5, 1]);
+  });
+  it('should return a [1]', () => {
+    var result = nonameTasks.destroyer([3, 5, 1, 2, 2], 2, 3, 5);
+    expect(result).to.have.lengthOf(1).and.to.include.members([1]);
+  });
+  it('should return a []', () => {
+    var result = nonameTasks.destroyer([2, 3, 2, 3], 2, 3);
+    expect(result).to.have.lengthOf(0);
+  });
+  it('should return a ["hamburger"]', () => {
+    var result = nonameTasks.destroyer(["tree", "hamburger", 53], "tree", 53);
+    expect(result).to.have.lengthOf(1).and.to.include.members(["hamburger"]);
+  });
+  it('should return a ["hamburger"]', () => {
+    var result = nonameTasks.destroyer(["tree", "hamburger", 53], "tree", 53);
+    expect(result).to.have.lengthOf(1).and.to.include.members(["hamburger"]);
+  });
+  it('should return a [12,92,65]', () => {
+    var result = nonameTasks.destroyer(["possum", "trollo", 12, "safari", "hotdog", 92, 65, "grandma", "bugati", "trojan", "yacht"], "yacht", "possum", "trollo", "safari", "hotdog", "grandma", "bugati", "trojan");
+    expect(result).to.have.lengthOf(3).and.to.include.members([12,92,65]);
+  });
+});
