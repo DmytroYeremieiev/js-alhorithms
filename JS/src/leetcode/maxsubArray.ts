d30f245dc70202bcbc1faba41bfb06c745d0b752
DmytroYeremieiev <dmytro.yeremieiev@gmail.com>
2019-03-28 17:46:39 -0700

    leet code

--- JS/src/leetcode/maxsubArray.ts
+++ JS/src/leetcode/maxsubArray.ts
@@ -8,15 +8,19 @@ Explanation: [4,-1,2,1] has the largest sum = 6. */
  * @return {number}
  */
 export function maxsubArray(nums: number[]) {
-  let min = 0, max = 0, accum = 0, curr, next;
+  let min = nums[0], max = nums[0], accum = 0, curr, next;
   //[-2, -1]; => -1;
   //[-2, -3, -1]; => -1;
   //[8,-19,5,-4,20]; => 21
   for (let i = 0; i < nums.length; i++) {
     curr = nums[i];
     next = nums[i+1];
-    if (curr < min) min = curr;
-    if (curr > max) max = curr;
+    if (curr < min) {
+      min = curr;
+    }
+    if (curr > max) {
+      max = curr;
+    }
 
     accum += curr;
     console.log(`curr: ${curr},   min: ${min}, max: ${max}, accum: ${accum}`);
@@ -24,12 +28,11 @@ export function maxsubArray(nums: number[]) {
     if (accum > max) {
       max = accum;
     }
-    if (accum <= min) {
+    if (accum < min) {
       accum = 0;
     } else {
       min = accum;
     }
-
   }
   return max;
 }
