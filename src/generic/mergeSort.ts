c451f230dc863505df3ae0002abbb517912dc371
DmytroYeremieiev <dmytro.yeremieiev@gmail.com>
2020-08-22 15:53:13 +0300

    - mergeSort refactoring

--- src/generic/mergeSort.ts
+++ src/generic/mergeSort.ts
@@ -1,27 +1,32 @@
-export function mergeSort(arr: Array<number>): Array<number> {
+export function mergeSort<T>(arr: Array<T>, selector: (item: T) => T): Array<T> {
   console.log('subproblem', arr);
   if (arr.length === 1) {
     return arr;
   }
-  const result = [];
-  const leftPartArr = mergeSort(arr.slice(0, arr.length / 2));
-  const rightPartArr = mergeSort(arr.slice(arr.length / 2, arr.length));
+  const result = Array(arr.length);
+  const leftPartArr = mergeSort(arr.slice(0, arr.length / 2), selector);
+  const rightPartArr = mergeSort(arr.slice(arr.length / 2, arr.length), selector);
   let l_Indx = 0,
     r_Indx = 0;
   for (let index = 0; index < arr.length; index++) {
-    const reachedEndOfLeft = leftPartArr.length <= l_Indx;
-    const reachedEndOfRight = rightPartArr.length <= r_Indx;
+    const leftPartArrCurrItem = leftPartArr[l_Indx],
+      leftPartArrSelectedProp = selector(leftPartArrCurrItem),
+      reachedEndOfLeft = leftPartArr.length <= l_Indx;
 
-    if ((leftPartArr[l_Indx] < rightPartArr[r_Indx] && !reachedEndOfLeft) || reachedEndOfRight) {
-      result[index] = leftPartArr[l_Indx];
+    const rightPartArrCurrItem = rightPartArr[r_Indx],
+      rightPartArrSelectedProp = selector(rightPartArrCurrItem),
+      reachedEndOfRight = rightPartArr.length <= r_Indx;
+
+    if ((leftPartArrSelectedProp < rightPartArrSelectedProp && !reachedEndOfLeft) || reachedEndOfRight) {
+      result[index] = leftPartArrCurrItem;
 
       if (!reachedEndOfLeft) {
         l_Indx++;
       }
       continue;
     }
-    if ((leftPartArr[l_Indx] >= rightPartArr[r_Indx] && !reachedEndOfRight) || reachedEndOfLeft) {
-      result[index] = rightPartArr[r_Indx];
+    if ((leftPartArrSelectedProp >= rightPartArrSelectedProp && !reachedEndOfRight) || reachedEndOfLeft) {
+      result[index] = rightPartArrCurrItem;
 
       if (!reachedEndOfRight) {
         r_Indx++;
@@ -32,5 +37,6 @@ export function mergeSort(arr: Array<number>): Array<number> {
   return result;
 }
 
-let r = mergeSort([5, 3, 6, 2, 8, 1, 4, 7]);
-console.log('r', r);
+// const r = mergeSort([8, 3, 6, 2, 8, 1, 4, 7], item => item);
+// const r = mergeSort([5, 3, 6, 2, 8, 1, 4, 7], item => item);
+// console.log('r', r);
