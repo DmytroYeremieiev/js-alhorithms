00f8f2b4156186ad0116a0a61b1c8b792f010445
DmytroYeremieiev <dmytro.yeremieiev@gmail.com>
2020-08-22 15:29:57 +0300

    - formatted

--- src/generic/mergeSortAndCountInversions.ts
+++ src/generic/mergeSortAndCountInversions.ts
@@ -1,18 +1,11 @@
-export function countInversions(
-  arr: Array<number>
-): { arr: Array<number>; inversions: number } {
-  console.log("subproblem", arr);
+export function countInversions(arr: Array<number>): { arr: Array<number>; inversions: number } {
+  console.log('subproblem', arr);
   if (arr.length === 1) {
     return { arr: arr, inversions: 0 };
   }
   const resultArr = Array.of(6);
-  const { arr: leftPartArr, inversions: leftSideInversions } = countInversions(
-    arr.slice(0, arr.length / 2)
-  );
-  const {
-    arr: rightPartArr,
-    inversions: rightSideInversions,
-  } = countInversions(arr.slice(arr.length / 2, arr.length));
+  const { arr: leftPartArr, inversions: leftSideInversions } = countInversions(arr.slice(0, arr.length / 2));
+  const { arr: rightPartArr, inversions: rightSideInversions } = countInversions(arr.slice(arr.length / 2, arr.length));
   let l_Indx = 0,
     r_Indx = 0;
   let splitSideInversions = 0;
@@ -20,20 +13,14 @@ export function countInversions(
     const reachedEndOfLeft = leftPartArr.length <= l_Indx;
     const reachedEndOfRight = rightPartArr.length <= r_Indx;
 
-    if (
-      (leftPartArr[l_Indx] < rightPartArr[r_Indx] && !reachedEndOfLeft) ||
-      reachedEndOfRight
-    ) {
+    if ((leftPartArr[l_Indx] < rightPartArr[r_Indx] && !reachedEndOfLeft) || reachedEndOfRight) {
       resultArr[index] = leftPartArr[l_Indx];
       if (!reachedEndOfLeft) {
         l_Indx++;
       }
       continue;
     }
-    if (
-      (leftPartArr[l_Indx] >= rightPartArr[r_Indx] && !reachedEndOfRight) ||
-      reachedEndOfLeft
-    ) {
+    if ((leftPartArr[l_Indx] >= rightPartArr[r_Indx] && !reachedEndOfRight) || reachedEndOfLeft) {
       resultArr[index] = rightPartArr[r_Indx];
       if (!reachedEndOfLeft) {
         splitSideInversions += leftPartArr.length - l_Indx;
@@ -52,4 +39,4 @@ export function countInversions(
 
 // const inversions = countInversions([6, 5, 4, 3, 2, 1]); // 15
 const inversions = countInversions([1, 3, 5, 2, 4, 6]); // 3
-console.log("inversions", inversions);
+console.log('inversions', inversions);
