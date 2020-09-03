a37f5fc3a03fd40c7eb7dc387169737eba2e9002
DmytroYeremieiev <dmytro.yeremieiev@gmail.com>
2020-09-03 15:59:53 +0300

    5.1 Find Average element

--- src/Algorithms Elluminated Part1/5.1-FindAverageElement.ts
+++ src/Algorithms Elluminated Part1/5.1-FindAverageElement.ts
@@ -12,7 +12,7 @@ export function FindAverageElement(arr: Array<number>): number {
   }
   const average = (maxVal + minVal) / 2;
 
-  console.log(`min: ${minVal}, max: ${maxVal}, ${average}`);
+  console.log(`min: ${minVal}, max: ${maxVal}, average: ${average}`);
 
   let averageEl = arr[0];
   let average_diff = maxVal - minVal;
@@ -29,6 +29,11 @@ export function FindAverageElement(arr: Array<number>): number {
   return averageEl;
 }
 
-// console.log('r1', FindAverageElement([3, 8, 2, 5, 1, 4, 7, 6]));
-// console.log('r2', FindAverageElement([3, 8, 2, 5, 1, 4, 7, 6, 9, 10, 11, 12, 13, 14, 16]));
-console.log('r2', FindAverageElement([14, 3, 13, 8, 9, 2, 12, 5, 1, 11, 4, 7, 6, 10, 16]));
+// console.log('r1: ', FindAverageElement([3, 8, 2, 5, 1, 4, 7, 6])); //  4 or 5
+// console.log('r2: ', FindAverageElement([3, 8, 2, 5, 1, 4, 7, 6, 9, 10, 11, 12, 13, 14, 16])); // 8
+// console.log('r3: ', FindAverageElement([14, 3, 13, 8, 9, 2, 12, 5, 1, 11, 4, 7, 6, 10, 16])); // 8
+
+console.log('r4: ', FindAverageElement([1, 20, 3, 4, 8])); // 4
+// console.log('r5: ', FindAverageElement([6, 8, 9, 2, 3])); // 3
+// console.log('r6: ', FindAverageElement([3, 2, 8, 9, 5, 1, 10, 4, 7, 6])); // 5 or 6
+// console.log('r7: ', FindAverageElement([6, 8, 9, 2])); // 6 or 8
