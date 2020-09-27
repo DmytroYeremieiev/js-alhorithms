bc1932c43665aad9b767b11605bbff87ae972c84
DmytroYeremieiev <dmytro.yeremieiev@gmail.com>
2020-09-27 15:20:21 +0300

    fixed partition logic

--- src/Algorithms Elluminated Part1/5-Partition-spec.ts
+++ src/Algorithms Elluminated Part1/5-Partition-spec.ts
@@ -10,6 +10,11 @@ describe('Partition', () => {
     // expect(result).to.have.members([1, 2, 3, 5, 8, 4, 7, 6]);
     expect(result).to.have.ordered.members([1, 2, 3, 5, 8, 4, 7, 6]);
   });
+  it('Partition([3, 8, 2, 5, 1, 4, 7, 6], 0) should return [2, 3, 8, 5, 1, 4, 7, 6].', () => {
+    const result = Partition([3, 8, 2, 5, 1, 4, 7, 6], 2); // 2, 8, 3, 5, 1, 4 ,7 ,6
+    console.log('result', result);
+    expect(result).to.have.ordered.members([1, 2, 3, 5, 8, 4, 7, 6]);
+  });
   it('Partition([3, 8, 5, 4, 7, 6], 0) should return [3, 8, 5, 4, 7, 6].', () => {
     const result = Partition([3, 8, 5, 4, 7, 6], 0);
     console.log('result', result);
--- src/Algorithms Elluminated Part1/5-Partition.ts
+++ src/Algorithms Elluminated Part1/5-Partition.ts
@@ -1,18 +1,27 @@
 export function Partition(arr: Array<number>, pivotIndex: number): Array<number> {
+  console.log('before pre-processing step', arr, `,  pivotIndex: ${pivotIndex}`);
+
   let i = 1;
-  let temp = null;
+  let temp = arr[0];
+  arr[0] = arr[pivotIndex];
+  arr[pivotIndex] = temp;
+  pivotIndex = 0;
+  console.log('after pre-processing step', arr, `,  pivotIndex: ${pivotIndex}`);
   for (let j = 1; j < arr.length; j++) {
     if (arr[j] < arr[pivotIndex]) {
+      console.log('if arr[j] < arr[pivotIndex]', arr, `arr[j]: ${arr[j]}, j: ${j}, i: ${i}`);
       temp = arr[i];
       arr[i] = arr[j];
       arr[j] = temp;
       i += 1;
     }
   }
+  console.log('after look: ', arr, i);
   temp = arr[i - 1];
   arr[i - 1] = arr[pivotIndex];
   arr[pivotIndex] = temp;
+  console.log('final result: ', arr, i);
   return arr;
 }
 
-console.log('r1', Partition([3, 8, 2, 5, 1, 4, 7, 6], 0));
+console.log('r1', Partition([3, 8, 2, 5, 1, 4, 7, 6], 2));
