32224f6137d969cad20fde079003369cc34c6bd3
DmytroYeremieiev <dmytro.yeremieiev@gmail.com>
2021-08-08 11:00:25 +0300

    Implementing  Heap data structure - logging

--- src/AbstractDataStructures/Heap.ts
+++ src/AbstractDataStructures/Heap.ts
@@ -58,8 +58,8 @@ class Heap<T> {
   printLevel(level: number, total_levels: number) {
     const level_size = Math.pow(2, level);
     const start = level_size - 1;
-    console.log(`level ${level}, starts at position: ${start}, size: ${level_size}`);
-    const elements = this.bin_tree_arr.slice(start, start + level_size);
+    // console.log(`level ${level}, starts at position: ${start}, size: ${level_size}`);
+    const keys = this.bin_tree_arr.slice(start, start + level_size);
     let offset = null;
     let distance = null;
     if (level === 0) {
@@ -69,21 +69,28 @@ class Heap<T> {
       distance = Math.pow(2, total_levels - level) - 1;
       offset = Math.pow(2, total_levels - level - 1) - 1;
     }
-    console.log(`distance: ${distance}, offset: ${offset}`);
-    let out_str = `${this.getSpaces(offset)}`;
+    // console.log(`distance: ${distance}, offset: ${offset}`);
     const latestEl = this.bin_tree_arr[this.bin_tree_arr.length - 1];
-    const maxKeySpace = (Math.log(latestEl) * Math.LOG10E + 1) | 0;
-    for (let i = 0; i < elements.length; i++) {
-      const element = elements[i];
-      const space = i === elements.length - 1 ? this.getSpaces(offset) : this.getSpaces(distance);
-      out_str += element + space;
+    const maxKeyLength = (Math.log(latestEl) * Math.LOG10E + 1) | 0;
+    let out_str = `${this.fillSpace(offset, maxKeyLength)}`;
+    for (let i = 0; i < keys.length; i++) {
+      const element = keys[i];
+      const space =
+        i === keys.length - 1 ? this.fillSpace(offset, maxKeyLength) : this.fillSpace(distance, maxKeyLength);
+      out_str += this.wrapKeyInSpace(element, maxKeyLength) + space;
     }
     console.log(out_str);
   }
-  getSpaces(count: number): string {
+  wrapKeyInSpace(key: number, maxKeyLength: number) {
+    const keyLength = (Math.log(key) * Math.LOG10E + 1) | 0;
+    const offset = maxKeyLength - keyLength;
+    return `${this.fillSpace(offset, 1, '0')}${key}`;
+  }
+  fillSpace(count: number, multiplier: number, filler = '–'): string {
     let res = '';
-    for (let i = 0; i < count; i++) {
-      res += '—';
+
+    for (let i = 0; i < count * multiplier; i++) {
+      res += filler;
     }
     return res;
   }
