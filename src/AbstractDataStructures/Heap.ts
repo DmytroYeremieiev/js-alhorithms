f7eb7bf2f7e4817d0a10521d68e0b911b83665f4
DmytroYeremieiev <dmytro.yeremieiev@gmail.com>
2021-08-09 16:14:45 +0300

    Implementing  Heap data structure - fixes

--- src/AbstractDataStructures/Heap.ts
+++ src/AbstractDataStructures/Heap.ts
@@ -9,7 +9,7 @@ class Heap<T> {
       const el = array[i];
       this.insert(el);
     }
-    this.log();
+    // this.log();
   }
   private get_parent_position(n: number): number {
     if (n === 0) return -1;
@@ -77,41 +77,53 @@ class Heap<T> {
     }
   }
   log() {
-    const total_levels = Math.ceil(Math.log2(this.bin_tree_arr.length));
+    let l = 0;
+    const total_levels = Math.floor(Math.log2(this.bin_tree_arr.length));
     console.log(
-      `...logging, total length: ${this.bin_tree_arr.length} and heap levels ${total_levels} `,
-      `inner array: `,
-      JSON.stringify(this.bin_tree_arr)
+      `Total heap levels ${total_levels}, inner array(${this.bin_tree_arr.length}): ${JSON.stringify(
+        this.bin_tree_arr
+      )}`
     );
     const sorted = Array.from(this.bin_tree_arr).sort((a, b) => a - b);
     const maxElement = sorted[sorted.length - 1];
     const maxKeyLength = (Math.log(maxElement) * Math.LOG10E + 1) | 0;
-    while (l < total_levels) {
+    while (l <= total_levels) {
       this.printLevel(l, total_levels, maxKeyLength);
       l++;
     }
   }
   printLevel(level: number, total_levels: number, maxKeyLength: number) {
+    if (total_levels === 0) {
+      console.log(`––– ${this.bin_tree_arr[0]} –––`);
+      return;
+    }
     const level_size = Math.pow(2, level);
     const start = level_size - 1;
-    // console.log(`level ${level}, starts at position: ${start}, size: ${level_size}`);
+    // console.log(
+    //   `......level ${level}, starts at position: ${start}, size: ${level_size}, total_levels: ${total_levels}, maxKeyLength: ${maxKeyLength}`
+    // );
     const keys = this.bin_tree_arr.slice(start, start + level_size);
-    let offset = null;
-    let distance = null;
+
+    let offset = 0;
+    let distance = 0;
+
     if (level === 0) {
+      offset = Math.pow(2, total_levels - level) - 1;
       distance = 0;
-      offset = Math.pow(2, total_levels - level - 1) - 1;
-    } else {
-      distance = Math.pow(2, total_levels - level) - 1;
-      offset = Math.pow(2, total_levels - level - 1) - 1;
+    } else if (total_levels !== level) {
+      distance = Math.pow(2, total_levels - level + 1) - 1;
+      offset = Math.pow(2, total_levels - level) - 1;
+    } else if (total_levels === level) {
+      offset = 0;
+      distance = Math.pow(2, total_levels - level);
     }
-    // console.log(`distance: ${distance}, offset: ${offset}, keys: ${keys.length}`);
+    // console.log(`......distance: ${distance}, offset: ${offset}, keys: ${keys.length}`);
     let out_str = `${this.fillSpace(offset, maxKeyLength + 2)}`;
     const fillerArray = Array.from(Array(level_size - keys.length));
     const _keys = [...keys, ...fillerArray];
     for (let i = 0; i < _keys.length; i++) {
       const element = _keys[i];
-      const between = this.fillSpace(i === keys.length - 1 ? 0 : distance, maxKeyLength + 2);
+      const between = this.fillSpace(i === _keys.length - 1 ? 0 : distance, maxKeyLength + 2);
       const node = this.wrapKeyInSpace(element ?? '', maxKeyLength, element ? '0' : '–');
       const nodeStr = element ? ` ${node} ` : `–${node}–`;
       out_str += `${nodeStr}${between}`;
@@ -134,6 +146,7 @@ class Heap<T> {
 }
 
 const heap = new Heap([4, 2, 8, 9, 4, 12, 9, 11, 13], el => el);
+heap.insert(11);
 heap.insert(1);
 heap.insert(10);
 heap.insert(16);
